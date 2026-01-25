136ed1b3be167af2c2a9479a7c6ab521e9d1885e
Jnayna 🐾 Jinene Ben Said <101910279+Jinene@users.noreply.github.com>
2026-01-26 00:23:51 +0100

    Add JWT authentication and authorization middleware

--- src/SupportTicket.API/Program.cs
+++ src/SupportTicket.API/Program.cs
@@ -1,1 +1,18 @@
+builder.Services.AddAuthentication("Bearer")
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        IssuerSigningKey =
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+            )
+    };
+});
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseMiddleware<TenantMiddleware>();
 
