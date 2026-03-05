806e9bcbd237d108284c15df11a303cdd1a0a129
Jnayna 🐾 Jinene Ben Said <101910279+Jinene@users.noreply.github.com>
2026-03-06 00:17:07 +0100

    Update TicketDto.cs

--- src/SupportTicket.Application/ DTOs/TicketDto.cs
+++ src/SupportTicket.Application/ DTOs/TicketDto.cs
@@ -7,5 +7,9 @@ public class TicketDto
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public TicketPriority Priority { get; set; }
+
+    # public string Title { get; set; } = string.Empty;
+    # public string Description { get; set; } = string.Empty;
+    # public TicketPriority Priority { get; set; }
 }
 
