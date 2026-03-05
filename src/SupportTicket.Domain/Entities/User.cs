8e0e9c8c991d5a51223d7176109102125536faca
Jnayna 🐾 Jinene Ben Said <101910279+Jinene@users.noreply.github.com>
2026-03-06 00:17:37 +0100

    Update User.cs

--- src/SupportTicket.Domain/Entities/User.cs
+++ src/SupportTicket.Domain/Entities/User.cs
@@ -10,3 +10,13 @@ public class User
     public string Role { get; set; } = "User"; // Admin, Support, User
 }
 
+#public class User
+{
+    public Guid Id { get; set; }
+    public Guid TenantId { get; set; }
+
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = "User"; // Admin, Support, User
+}
+#
